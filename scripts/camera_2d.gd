1b5304395b3ee235fc00b163f57cb28e50a86626
yannis-munayi <ytmunayi@gmail.com>
2025-01-23 20:03:05 -0500

    Fixed the bottom killzone, working on the top 1 when fullscreened

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -2,7 +2,7 @@ extends Camera2D
 @onready var left_kill_zone: Area2D = $"Left Kill Zone"
 @onready var right_kill_zone: Area2D = $"Right Kill Zone"
 @onready var top_kill_zone: Area2D = $"../../../Kill Zones/Top Kill Zone"
-@onready var player: CharacterBody2D = $"../../Player_2"
+@onready var player: CharacterBody2D = $"../../MultPlayer"
 @onready var camera_base_player: CharacterBody2D = $".."
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,7 +11,11 @@ func _process(delta: float) -> void:
 
 	# changes the camera killzone depending on if the screen is windowed/maximed
 	if get_window().mode == 2:
-		print(camera_base_player.position.x - player.position.x)
+		# Adds the bottom kill zone
+		print(player.GRA.x)
+		if player.GRA.x >= 156 or (player.GRA.x <=84 and player.GRA.x != 1):
+			died()
+			
 		if round(camera_base_player.position.x - player.position.x) <= -344:
 			died()
 		if round(player.position.x - camera_base_player.position.x) <= -344:
@@ -19,6 +23,10 @@ func _process(delta: float) -> void:
 		
 		
 	elif get_window().mode == 0:
+		# Adds the bottom kill zone
+		if player.GRA.x >= 70:
+			died()
+			
 		if round(camera_base_player.position.x - player.position.x) <= -210:
 			died()
 		if round(player.position.x - camera_base_player.position.x) <= -210:
