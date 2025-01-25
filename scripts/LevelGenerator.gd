081146a5a37a651dccf794c89d5f8a52e19afff6
yannis-munayi <ytmunayi@gmail.com>
2025-01-24 21:32:04 -0500

    Fixed the Background generating without the midground

--- scripts/LevelGenerator.gd
+++ scripts/LevelGenerator.gd
@@ -29,7 +29,7 @@ var premade_platform_x_range: Vector2 = Vector2(0, 0)  # Adjust this to match yo
 func generate_level():
 	var x_pos = last_generated_x
 	if get_window().mode == 0:
-		min_row = 4
+		min_row = -7
 		max_row = 4
 		#rint(player.position.x) # -7
 	elif get_window().mode == 2:
